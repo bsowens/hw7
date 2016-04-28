81708f66ae7bbe7af289bb2b403cfef618e387dc
bsowens <ben@wireless1x-155-41-126-101.bu.edu>
2016-04-28 15:34:08 -0400

    n ;l

--- hw7.py
+++ hw7.py
@@ -51,6 +51,7 @@ def repeated_random(k):
             if residue < best_so_far:
                 best_so_far = residue
         best_res_set.append(best_so_far)
+        print(best_res_set)
     return best_res_set
 
 
@@ -79,44 +80,40 @@ def gradient_descent1(k):
     return best_res_set
 
 
-
-
-
-def gradient_descent(l,k):
-    n = len(l)
-    randListGen = lambda n: [random.choice([-1,1]) for x in range(0,n)]
-    solution1 = [random.choice([-1,1]) for x in range(0,n)]
-    residue1 = residue(solution1,l)
-    for x in range(0,k):
-        i = random.randint(0,n-1)
-        j = random.randint(0,n-1)
-        solution2 = solution1
-
-        solution2[i] = solution2[i] * -1
-
-        solution2[j] = solution2[j] * random.choice([1,-1])
-        residue2 = residue(solution2,l)
-        if residue2 < residue1:
-            solution1 = solution2
-            residue1 = residue(solution1,l)
-    return solution1
-
 def temp(i):
-    return pow(10,10)*pow(0.8,(i/300))
+    return pow(10,10)*pow(0.8,(i/300.0))
 
 
 
-#Function for weighted random pick from https://www.safaribooksonline.com/library/view/python-cookbook-2nd/0596007973/ch04s22.html
 
-def random_pick(some_list, probabilities):
-    x = random.uniform(0, 1)
-    cumulative_probability = 0.0
-    for item, item_probability in zip(some_list, probabilities):
-        cumulative_probability += item_probability
-        if x < cumulative_probability: break
-    return item
+def simulated_annealing(k):
 
-def simulated_annealing(l,k):
+    best_res_set = []
+    for on_list in range(0,50):
+        l = randSet()
+        best_so_far = 999999999999999
+        for j in range(0,k):
+            residue1 = abs(sum(l))
+            x = random.randint(0,99)
+            y = random.randint(0,99)
+            l[x] = l[x] * -1
+            l[y] = l[y] * random.choice([1,-1])
+            residue2 = abs(sum(l))
+            p = math.exp(-( residue1 - residue2  ) / temp(j)   )
+            randFloat = random.uniform(0,1)
+            if residue2 < best_so_far or (randFloat < p and residue2 > best_so_far):
+                best_so_far = residue2
+            
+            
+                
+        best_res_set.append(best_so_far)
+    return best_res_set
+
+
+
+
+    
+    '''
     n = len(l)
     randListGen = lambda n: [random.choice([-1,1]) for x in range(0,n)]
     solution1 = randListGen(n)
@@ -141,7 +138,7 @@ def simulated_annealing(l,k):
             residue1 = residue(solution1,l)
     return solution1
 
-
+'''
 def testKK():
     rand = randSets()
     test = []
